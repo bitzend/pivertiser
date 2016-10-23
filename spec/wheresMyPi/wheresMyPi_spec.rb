46f8c425eed4cc30bcf170443e9b38751e02f398
Douglas Osborn <dz@bitzend.net>
2016-10-23 12:58:34 -0700

    Update wheresMyPi_spec.rb

--- spec/wheresMyPi/wheresMyPi_spec.rb
+++ spec/wheresMyPi/wheresMyPi_spec.rb
@@ -1,1 +1,21 @@
+module WheresMyPi
+  JSON_HEADERS = { 'HTTP_ACCEPT' => 'application/json' }
 
+  describe App do
+    it 'says hello' do
+      get '/'
+      expect(last_response).to be_ok
+      expect(last_response.body).to match /Raspis we've heard from recently/
+    end
+
+    it 'serves JSON' do
+      get '/', nil, JSON_HEADERS
+      expect(last_response).to be_ok
+      expect(JSON.parse last_response.body).to eq (
+        {
+          'app' => 'WheresMyPi'
+        }
+      )
+    end
+  end
+end
